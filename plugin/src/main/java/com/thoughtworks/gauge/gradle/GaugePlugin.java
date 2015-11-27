a5d657a43e8ad8ee6d1333bf3f7b210a603944a9
Manu Sunny <manupsunny@gmail.com>
2015-11-27 20:01:11 +0530

    Adding GPL 3.0 public license

--- plugin/src/main/java/com/thoughtworks/gauge/gradle/GaugePlugin.java
+++ plugin/src/main/java/com/thoughtworks/gauge/gradle/GaugePlugin.java
@@ -1,3 +1,22 @@
+/*
+ *  Copyright 2015 Manu Sunny
+ *
+ *  This file is part of Gauge-gradle-plugin.
+ *
+ *  Gauge-gradle-plugin is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Gauge-gradle-plugin is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Gauge-gradle-plugin.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.thoughtworks.gauge.gradle;
 
 import org.gradle.api.Plugin;
@@ -7,11 +26,20 @@ import java.util.Map;
 
 public class GaugePlugin implements Plugin<Project> {
 
+    public static final String GAUGE = "gauge";
+    public static final String SPECS_DIR = "specsDir";
+    public static final String IN_PARALLEL = "inParallel";
+    public static final String TAGS = "tags";
+    public static final String ENV = "env";
+    public static final String NODES = "nodes";
+    public static final String ADDITIONAL_FLAGS = "additionalFlags";
+    public static final String CLASSPATH = "classpath";
+
     @Override
     public void apply(Project project) {
-        project.getExtensions().create("gauge", GaugeExtension.class);
+        project.getExtensions().create(GAUGE, GaugeExtension.class);
         pullCommandLineArguments(project);
-        project.getTasks().create("gauge", GaugeTask.class);
+        project.getTasks().create(GAUGE, GaugeTask.class);
     }
 
     private void pullCommandLineArguments(Project project) {
@@ -27,49 +55,49 @@ public class GaugePlugin implements Plugin<Project> {
     }
 
     private void setSpecsDir(Map<String, ?> properties, GaugeExtension extension) {
-        String specsDir = (String) properties.get("specsDir");
+        String specsDir = (String) properties.get(SPECS_DIR);
         if (specsDir != null) {
             extension.setSpecsDir(specsDir);
         }
     }
 
     private void setInParallel(Map<String, ?> properties, GaugeExtension extension) {
-        String inParallel = (String) properties.get("inParallel");
+        String inParallel = (String) properties.get(IN_PARALLEL);
         if (inParallel != null) {
             extension.setInParallel(inParallel.equals("true"));
         }
     }
 
     private void setTags(Map<String, ?> properties, GaugeExtension extension) {
-        String tags = (String) properties.get("tags");
+        String tags = (String) properties.get(TAGS);
         if (tags != null) {
             extension.setTags(tags);
         }
     }
 
     private void setEnv(Map<String, ?> properties, GaugeExtension extension) {
-        String env = (String) properties.get("env");
+        String env = (String) properties.get(ENV);
         if (env != null) {
             extension.setEnv(env);
         }
     }
 
     private void setNodes(Map<String, ?> properties, GaugeExtension extension) {
-        String nodes = (String) properties.get("nodes");
+        String nodes = (String) properties.get(NODES);
         if (nodes != null) {
             extension.setNodes(Integer.parseInt(nodes));
         }
     }
 
     private void setAdditionalFlags(Map<String, ?> properties, GaugeExtension extension) {
-        String flags = (String) properties.get("additionalFlags");
+        String flags = (String) properties.get(ADDITIONAL_FLAGS);
         if (flags != null) {
             extension.setAdditionalFlags(flags);
         }
     }
 
     private void setClasspath(Map<String, ?> properties, GaugeExtension extension) {
-        String classpath = (String) properties.get("classpath");
+        String classpath = (String) properties.get(CLASSPATH);
         if (classpath != null) {
             extension.setClasspath(classpath);
         }
